fda35942ad26d062ed739061a0047ea326566486
Elias Kahl <contact@elias.works>
2022-04-05 19:41:36 +0000

    feat: 2d array to svg heatmap

--- src/AdvancedHeatmap.ts
+++ src/AdvancedHeatmap.ts
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import { html, svg, css, LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 
 export class AdvancedHeatmap extends LitElement {
@@ -10,18 +10,45 @@ export class AdvancedHeatmap extends LitElement {
     }
   `;
 
-  @property({ type: String }) title = 'Hey there';
+  @property({ type: String })
+  color: String = '#ff0000';
 
-  @property({ type: Number }) counter = 5;
+  @property({ type: Number })
+  gutter: number = 0.05;
 
-  __increment() {
-    this.counter += 1;
+  @property({ attribute: false })
+  data: Array<Array<Number>> = [];
+
+  __nRows() {
+    return this.data.length;
+  }
+
+  __nCols() {
+    if (this.__nRows() === 0) {
+      return 0;
+    }
+    return this.data[0].length;
+  }
+
+  renderRow(row: Array<Number>, y: Number) {
+    return svg`${row.map(
+      (value, i) => svg`
+      <rect
+        x="${i}"
+        y="${y}"
+        width="${1 - this.gutter}"
+        height="${1 - this.gutter}"
+        fill="${this.color}"
+        opacity="${value}"/>
+      `
+    )}`;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <svg version="1.1" viewBox="0 0 ${this.__nCols()} ${this.__nRows()}">
+        ${this.data.map((row, i) => this.renderRow(row, i))}
+      </svg>
     `;
   }
 }
