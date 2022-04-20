9f87631683b12fe30d82e41933c5d86973867ef8
Elias Kahl <contact@elias.works>
2022-04-20 11:04:57 +0000

    feat!: remove non functional demo buttons

--- demo/app.ts
+++ demo/app.ts
@@ -1,7 +1,6 @@
 import { html, render } from 'lit';
 import '../src/biowc-heatmap.js';
 import { fetchDemoData } from './demoData.js';
-import { BiowcHeatmap } from '../src/BiowcHeatmap.js';
 
 const demoData = await fetchDemoData();
 
@@ -25,17 +24,3 @@ render(
     `,
   document.querySelector('#demo') as HTMLElement
 );
-
-const heatmap = document.querySelector('biowc-heatmap') as BiowcHeatmap;
-const zoomInButton = document.querySelector('#zoom-in');
-const zoomOutButton = document.querySelector('#zoom-out');
-
-zoomInButton?.addEventListener('click', () => {
-  heatmap.zoomX += 0.5;
-  heatmap.zoomY += 0.5;
-});
-
-zoomOutButton?.addEventListener('click', () => {
-  heatmap.zoomX += 0.5;
-  heatmap.zoomY += 0.5;
-});
