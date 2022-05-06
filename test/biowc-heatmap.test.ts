cd80d32ec8565bbf6e7db7703d3e92d2ddc55047
Elias Kahl <contact@elias.works>
2022-05-06 08:50:46 +0000

    test: remove labels length mismatch error test

--- test/biowc-heatmap.test.ts
+++ test/biowc-heatmap.test.ts
@@ -1,7 +1,6 @@
 import { html } from 'lit';
-import { fixture, expect, assert } from '@open-wc/testing';
-import { stub, match } from 'sinon';
-import { BiowcHeatmap, Side } from '../src/BiowcHeatmap.js';
+import { fixture, expect } from '@open-wc/testing';
+import { BiowcHeatmap } from '../src/BiowcHeatmap.js';
 import '../src/biowc-heatmap.js';
 
 describe('BiowcHeatmap', () => {
@@ -16,32 +15,5 @@ describe('BiowcHeatmap', () => {
     );
 
     await expect(el).shadowDom.to.be.accessible();
-  });
-
-  it(`logs error when labels and data lengths mismatch`, async () => {
-    const consoleErrorStub = stub(console, 'error');
-    const el = await fixture<BiowcHeatmap>(
-      html`<biowc-heatmap
-        .data=${[
-          [1, 1],
-          [1, 1],
-        ]}
-      ></biowc-heatmap>`
-    );
-
-    for (const side of Object.values(Side)) {
-      el.labels = {
-        [side]: [side],
-      };
-
-      // eslint-disable-next-line no-await-in-loop
-      await el.updateComplete;
-
-      assert(
-        consoleErrorStub.calledWith(match((err: string) => err.includes(side)))
-      );
-    }
-
-    consoleErrorStub.restore();
   });
 });
