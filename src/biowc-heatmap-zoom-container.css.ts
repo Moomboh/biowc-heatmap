b55b90df0f8571038142b15f135372d11c881d62
Elias Kahl <contact@elias.works>
2022-05-24 08:39:15 +0000

    perf: transform layer scroll perf hack

--- src/biowc-heatmap-zoom-container.css.ts
+++ src/biowc-heatmap-zoom-container.css.ts
@@ -8,6 +8,7 @@ export default css`
 
 ::slotted(*) {
     overflow: hidden;
+    transform: translateZ(0);
     width: var(--biowc-heatmap-zoom-width, 100%);
     height: var(--biowc-heatmap-zoom-height, 100%);
 }
